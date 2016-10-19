b7089f62e1057b9d4485239d632f65597dea8e59
fanhaofacai <fanhaofacai@gmail.com>
2016-10-19 10:16:12 +0800

    Update game_manager.js

--- chairman_files/game_manager.js
+++ chairman_files/game_manager.js
@@ -146,7 +146,7 @@ GameManager.prototype.move = function (direction) {
 //            maxscore = merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 32) self.won = true;
+          if (merged.value === 2048) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
